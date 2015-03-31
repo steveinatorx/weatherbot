7ea20f185c8de17a02d82c735193bb25165ff61b
steveinatorx <steveinatorx@gmail.com>
2015-03-31 12:50:55 -0700

    fat finger fix

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -35,7 +35,7 @@ module.exports = function(config) {
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
         //pick up config.js
-      '/app/scripts/*.js',  
+      'app/scripts/*.js',  
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
